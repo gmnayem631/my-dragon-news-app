c6a7349c201407dcaf4d445bc13a04b0250a3439
gmnayem631 <you@example.comgmnayem631@gmail.com>
2025-05-03 09:47:13 +0600

    added current date from date fns

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import logo from "../../assets/logo.png";
+import { format } from "date-fns";
 
 const Header = () => {
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="flex flex-col justify-center items-center gap-3">
       <img src={logo} alt="" />
-      <p>Journalism Without Fear or Favour</p>
+      <p className="text-accent">Journalism Without Fear or Favour</p>
+      <p>{format(new Date(), "EEEE, MMMM dd, yyyy")}</p>
     </div>
   );
 };
